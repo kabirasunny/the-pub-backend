a234c2e9428dfd605cfc9fdd3cbcf19a38b80db5
kabirasunny <sunnykabira5507@gmail.com>
2024-09-13 15:36:07 +0530

    I have changed  '@Table name value thepub_user

--- the-pub/src/main/java/com/thepub/models/ThepubUser.java
+++ the-pub/src/main/java/com/thepub/models/ThepubUser.java
@@ -7,9 +7,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Component
 @Entity
+@Table(name = "thepub_user")
 public class ThepubUser {
 
 	@Id
