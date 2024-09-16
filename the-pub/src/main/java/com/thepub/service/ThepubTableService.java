aa10f2134fcd97a3363a689d3d4386ecdea5cfac
kabirasunny <sunnykabira5507@gmail.com>
2024-09-16 15:26:45 +0530

    I have added new class service for table logic

---
+++ the-pub/src/main/java/com/thepub/service/ThepubTableService.java
@@ -0,0 +1,18 @@
+package com.thepub.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thepub.dao.ThepubTableRepo;
+import com.thepub.models.ThepubTable;
+
+@Service
+public class ThepubTableService {
+
+	@Autowired
+	private ThepubTableRepo repository;
+	
+	public ThepubTable saveTable(ThepubTable table) {
+		return this.repository.save(table);
+	}
+}
