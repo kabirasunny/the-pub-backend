0bc67b407acafd68563e086651c4b9672e7c5aa4
kabirasunny <sunnykabira5507@gmail.com>
2024-09-17 13:47:48 +0530

    auto update

--- the-pub/src/main/java/com/thepub/dao/ThepubTableRepo.java
+++ the-pub/src/main/java/com/thepub/dao/ThepubTableRepo.java
@@ -1,9 +1,11 @@
 package com.thepub.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.thepub.models.ThepubTable;
 
+@Repository
 public interface ThepubTableRepo extends CrudRepository<ThepubTable, Integer> {
 
 }
