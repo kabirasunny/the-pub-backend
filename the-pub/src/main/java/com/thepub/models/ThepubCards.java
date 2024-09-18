68ede98a56031daf8048b5478083d4352a0a5c91
kabirasunny <sunnykabira5507@gmail.com>
2024-09-18 14:49:39 +0530

    I have updated in annotation at id generationType.Auto to IDENTITY

--- the-pub/src/main/java/com/thepub/models/ThepubCards.java
+++ the-pub/src/main/java/com/thepub/models/ThepubCards.java
@@ -14,7 +14,7 @@ import jakarta.persistence.Table;
 public class ThepubCards {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String image;
 	private String title;
