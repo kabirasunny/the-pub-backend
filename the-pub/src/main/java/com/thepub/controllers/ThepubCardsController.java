92e58b795cbac9b6f6fdd25a7e8e3e37a45607ad
kabirasunny <sunnykabira5507@gmail.com>
2024-09-22 22:09:09 +0530

    I have changed in setCards method return class object to string

--- the-pub/src/main/java/com/thepub/controllers/ThepubCardsController.java
+++ the-pub/src/main/java/com/thepub/controllers/ThepubCardsController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.thepub.models.ThepubCards;
@@ -11,17 +12,18 @@ import com.thepub.service.ThepubCardsService;
 
 @RestController
 public class ThepubCardsController {
-	
+
 	@Autowired
 	private ThepubCardsService thePubCardsService;
+
 	@PostMapping("/allCards")
-	public ThepubCards setCards(@ModelAttribute ThepubCards cards) {
+	public String setCards(@ModelAttribute ThepubCards cards) {
 		ThepubCards card = this.thePubCardsService.setCards(cards);
-		return card;
+		return "Your card has been added successful !";
 	}
 
 	@GetMapping("/")
-	public Iterable<ThepubCards> getCards(){
+	public Iterable<ThepubCards> getCards() {
 		Iterable<ThepubCards> allCards = this.thePubCardsService.getCards();
 		return allCards;
 	}
