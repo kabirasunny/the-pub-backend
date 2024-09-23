b1d07ea757da398fb929e25b1841bfbb77ae9b2b
kabirasunny <sunnykabira5507@gmail.com>
2024-09-23 14:36:24 +0530

    I have added delCard method

--- the-pub/src/main/java/com/thepub/controllers/ThepubCardsController.java
+++ the-pub/src/main/java/com/thepub/controllers/ThepubCardsController.java
@@ -1,8 +1,10 @@
 package com.thepub.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +28,16 @@ public class ThepubCardsController {
 	public Iterable<ThepubCards> getCards() {
 		Iterable<ThepubCards> allCards = this.thePubCardsService.getCards();
 		return allCards;
+	}
+
+	@DeleteMapping("/deleteCard/{title}")
+	public String delCard(@PathVariable String title) {
+		System.out.println(title+"super hero");
+		try {
+			this.thePubCardsService.deleteCard(title);
+			return "Your card delete successfully";
+		} catch (Exception e) {
+			return "Your card not deleted! please try again";
+		}
 	}
 }
