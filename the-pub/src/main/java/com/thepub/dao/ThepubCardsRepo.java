7ffc368e6b175e3abf6c21593186e4ee5e08b1d3
kabirasunny <sunnykabira5507@gmail.com>
2024-09-23 14:37:16 +0530

    I have added method deletetitle and Query

--- the-pub/src/main/java/com/thepub/dao/ThepubCardsRepo.java
+++ the-pub/src/main/java/com/thepub/dao/ThepubCardsRepo.java
@@ -1,6 +1,8 @@
 package com.thepub.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.thepub.models.ThepubCards;
@@ -8,4 +10,6 @@ import com.thepub.models.ThepubCards;
 @Repository
 public interface ThepubCardsRepo extends CrudRepository<ThepubCards, Integer>{
 
+	 @Query(value = "DELETE FROM thepub_cards WHERE title = :title", nativeQuery = true)
+	public void deleteTitle(String title);
 }
