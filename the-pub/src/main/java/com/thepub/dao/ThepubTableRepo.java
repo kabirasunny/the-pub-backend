97eb4c0f6454d70f70bea187f15a252c21d5a4fc
kabirasunny <sunnykabira5507@gmail.com>
2024-09-24 15:17:44 +0530

    I have added new method findByNumber

--- the-pub/src/main/java/com/thepub/dao/ThepubTableRepo.java
+++ the-pub/src/main/java/com/thepub/dao/ThepubTableRepo.java
@@ -4,8 +4,11 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.thepub.models.ThepubTable;
+import java.util.List;
+
 
 @Repository
 public interface ThepubTableRepo extends CrudRepository<ThepubTable, Integer> {
 
+	public Iterable<ThepubTable> findByNumber(long number);
 }
